d762825b62464f6acf8fe85bcfeebc861df334d9
yundrod <31172421+yundrod@users.noreply.github.com>
2017-11-19 14:24:35 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,64 +1,70 @@
-#include <iostream>
-#include <math.h>
-#include <stdlib.h>
-#include <SFML/Graphics.hpp>
-#include <SFML/Graphics/Color.hpp>
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/System/Time.hpp>
+#include "clases.h"
+#include <string>
 
-#define PI 3.14159265
 
 using namespace std;
 using namespace sf;
 
+
 int main(){
-    RectangleShape canon;
-    canon.setOrigin(Vector2f(3,0));
-    canon.setSize(Vector2f(6,-20));
-    canon.setPosition(Vector2f(400,300));
+    string name;
+    RectangleShape line(Vector2f(800, 5)); //linea del suelo
+    line.setPosition(0,suelo);//posicion del suelo
+    cout<<"ingrese nombre del jugador";
+    cin>>name;
 
-    CircleShape player(10);
-    player.setPosition(390,290);
+    player player1(0.0,suelo-20,name);//
 
+    float aux1,angle, v_0;
     RenderWindow window(VideoMode(800, 600), "My render");
-
     while(window.isOpen()){
         Event event;
-        while (window.pollEvent(event))
-        {
-	// "close requested" event: we close the window
-        if (event.type == Event::Closed)
-            window.close();
-        if(Keyboard::isKeyPressed(Keyboard::Escape))
-            window.close();
-        if(Keyboard::isKeyPressed(Keyboard::Up)){
-
-            canon.rotate(-1);
-        }
-        if(Keyboard::isKeyPressed(Keyboard::Down)){
-
-            canon.rotate(1);
-        }
-        if(Keyboard::isKeyPressed(Keyboard::Left)){
-
-            canon.move(-1,0);
-            player.move(-1,0);
-        }
-        if(Keyboard::isKeyPressed(Keyboard::Right)){
-
-            canon.move(1,0);
-            player.move(1,0);
+        while (window.pollEvent(event)){
+            // "close requested" event: we close the window
+            if (event.type == Event::Closed){
+                window.close();
+            }
+            if(Keyboard::isKeyPressed(Keyboard::Escape)){
+                window.close();
+            }
         }
         // clear the window with black color
         window.clear(Color(0,0,0));
-        // draw everything here...
-        //window.draw(player);
-        window.draw(canon);
-        window.draw(player);
-        // end the current frame
-        window.display();
+        window.draw(player1.body);
+        window.draw(player1.canon);
+        window.draw(line);
+        window.draw(player1.id);
+        window.draw(player1.healbar);
+        window.draw(player1.angleview);
+        if(Keyboard::isKeyPressed(Keyboard::Space)){
+            cout << "Inserta el angulo: ";
+            cin >> angle;
+            cout << "Inserta la velocidad inicial: ";
+            cin >> v_0;
+            if (angle==90){
+                aux1=0;
+            }
+            else{
+                aux1=angle;
+            }
+            proyectil bomba(player1.body.getPosition().x,player1.body.getPosition().y,aux1,v_0);
+            while (bomba.C.y+5 <= suelo){
+                window.display();
+                window.draw(bomba.proy);
+                window.clear(Color(0,0,0));
+                window.draw(player1.body);
+                window.draw(player1.canon);
+                window.draw(bomba.proy);
+                window.draw(line);
+                bomba.movimiento();
+                sleep(bomba.t1);
+            }
         }
-  }
 
-  return 0;
+
+        window.display();
+        player1.movimiento();
+    }
+    cout<<"end of program";
+    return 0;
 }
